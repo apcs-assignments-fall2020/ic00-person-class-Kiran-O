8418610b7d3dd46064c269e1b7bd86794f61491e
Kiran-O <22kokelly@athenian.org>
2020-10-26 19:34:14 +0000

    Passed All Tests

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,45 @@
 public class Person {
-    
+    private String Firstname;
+    private String Lastname;
+
+    public Person(String x, String y){
+        Firstname = x;
+        Lastname = y; 
+    }
+
+    public void doSomething(){
+        System.out.println("I am watching Dino-King on TikTok at the moment");
+    }
+
+    public String toString(){
+        return (Firstname + " " + Lastname);
+    }
+
+    public String getFirstname(){
+        return (this.Firstname);
+
+    }
+
+    public void setFirstname (String Firstname){
+        if (Firstname.trim().length()!= 0){
+            this.Firstname = Firstname;
+        }
+    }
+
+
+    public String getLastname(){
+        return (this.Lastname);
+    }
+
+    public void setLastname (String Lastname){
+        if (Lastname.trim().length()!= 0){
+            this.Lastname = Lastname;
+        }
+    }
+
 }
 
-
-
+   
 
 
 
